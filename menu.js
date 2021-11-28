5f5460b95663bca340cdfedb4a169c7662deac33
camly2512 <93423055+camly2512@users.noreply.github.com>
2021-11-28 21:53:14 +0700

    Delete menu.js

--- menu.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded",function(){
-    var nut = document.querySelector('div.menu img');
-    var mobile = document.querySelector('ul');
-    nut.addEventListener('click',function(){
-        mobile.classList.toggle('open');
-    })
-})
